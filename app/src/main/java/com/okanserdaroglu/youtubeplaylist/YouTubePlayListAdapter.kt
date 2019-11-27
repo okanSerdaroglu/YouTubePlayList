3db0bb6dcf9c26cd93b92e9432700d1ef622ed0c
i_serdaroglu@thy.com <okanserdaroglu2@gmail.com>
2019-11-27 13:58:59 +0300

    gilde added for images

--- app/src/main/java/com/okanserdaroglu/youtubeplaylist/YouTubePlayListAdapter.kt
+++ app/src/main/java/com/okanserdaroglu/youtubeplaylist/YouTubePlayListAdapter.kt
@@ -1,18 +1,23 @@
 package com.okanserdaroglu.youtubeplaylist
 
 import Items
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.adapter_youtube_list.view.*
 
 
 class YouTubePlayListAdapter(var playList: List<Items>?) :
     RecyclerView.Adapter<YouTubePlayListAdapter.ViewHolder>() {
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
 
       return ViewHolder(LayoutInflater.from(parent.context).
           inflate(R.layout.adapter_youtube_list,parent,false))
@@ -39,7 +44,13 @@ class YouTubePlayListAdapter(var playList: List<Items>?) :
 
         fun setData(play: Items?, position: Int) {
 
-              textViewPlayListTitle.text = play?.snippet?.channelTitle
+              textViewPlayListTitle.text = play?.snippet?.channelTitle // play?.snippet?.thumbnails?.medium.url
+            Glide
+                .with(itemView.context)
+                .load(play?.snippet?.thumbnails?.medium?.url)
+                .centerCrop()
+                .placeholder(R.drawable.common_full_open_on_phone)
+                .into(imagePlayList)
 
         }
 
